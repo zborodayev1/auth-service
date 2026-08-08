ec279df2d949c9cded21d9c28116f90a94b59637
Zakhar Borodayev <zakharborodayev@gmail.com>
2026-08-08 23:22:37 +0500

    feat(auth): fix jwt isser bug

--- src/infrastructure/jwt/JwtClientAccessTokenService.ts
+++ src/infrastructure/jwt/JwtClientAccessTokenService.ts
@@ -4,8 +4,9 @@ import type {
 } from '@ports/ClientAccessTokenService'
 import { ServerConfig } from '@config/server/server'
 import { inject, injectable } from 'inversify'
-import jwt from 'jsonwebtoken'
+import jwt, { JsonWebTokenError } from 'jsonwebtoken'
 import { ValidationError } from '@shared/errors/ValidationError'
+import { UnauthorizedError } from '@shared/errors/UnauthorizedError'
 
 @injectable()
 export class JwtClientAccessTokenService implements ClientAccessTokenService {
@@ -16,24 +17,35 @@ export class JwtClientAccessTokenService implements ClientAccessTokenService {
 
   sign(clientId: string, sessionId: string): string {
     const expiresIn = this.config.jwtExpiresInString as jwt.SignOptions['expiresIn'] & string
-    return jwt.sign({ sub: clientId, sid: sessionId }, this.config.jwtSecret, { expiresIn })
+    return jwt.sign({ sub: clientId, sid: sessionId }, this.config.jwtSecret, {
+      expiresIn,
+      issuer: 'auth-system',
+    })
   }
 
   verify(token: string): ClientAccessTokenPayload {
-    const payload = jwt.verify(token, this.config.jwtSecret, {
-      algorithms: ['HS256'],
-      issuer: 'auth-system',
-    })
-    if (
-      typeof payload === 'string' ||
-      typeof payload.sub !== 'string' ||
-      typeof payload['sid'] !== 'string'
-    ) {
-      throw new ValidationError('Invalid token payload', 'INVALID_TOKEN_PAYLOAD')
-    }
-    return {
-      clientId: payload.sub,
-      sessionId: payload['sid'],
+    let payload: jwt.JwtPayload | string
+    try {
+      payload = jwt.verify(token, this.config.jwtSecret, {
+        algorithms: ['HS256'],
+        issuer: 'auth-system',
+      })
+      if (
+        typeof payload === 'string' ||
+        typeof payload.sub !== 'string' ||
+        typeof payload['sid'] !== 'string'
+      ) {
+        throw new ValidationError('Invalid token payload', 'INVALID_TOKEN_PAYLOAD')
+      }
+      return {
+        clientId: payload.sub,
+        sessionId: payload['sid'],
+      }
+    } catch (e) {
+      if (e instanceof JsonWebTokenError) {
+        throw new UnauthorizedError('Invalid or expired token', 'INVALID_TOKEN')
+      }
+      throw e
     }
   }
 }
